6c48e2baa5540e7a03ec2c47799f8058882f8d94
Carlo10K <carlosortegadiaz50@gmail.com>
2023-01-03 17:26:04 -0600

    Post,Delete,Put

--- src/main/java/com/fundamentals/springboot/fundamentals/controller/UserRestController.java
+++ src/main/java/com/fundamentals/springboot/fundamentals/controller/UserRestController.java
@@ -1,23 +1,38 @@
 package com.fundamentals.springboot.fundamentals.controller;
 
+import com.fundamentals.springboot.fundamentals.caseuse.CreateUser;
+import com.fundamentals.springboot.fundamentals.caseuse.DeleteUser;
 import com.fundamentals.springboot.fundamentals.caseuse.GetUser;
+import com.fundamentals.springboot.fundamentals.caseuse.UpdateUser;
 import com.fundamentals.springboot.fundamentals.entity.User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserRestController {
-    private GetUser getUser;
+    private final GetUser getUser;
+    private final CreateUser createUser;
 
-    public UserRestController(GetUser getUser) {
+    private final DeleteUser deleteUser;
+    private final UpdateUser updateUser;
+
+    public UserRestController(GetUser getUser, CreateUser createUser, DeleteUser deleteUser, UpdateUser updateUser) {
         this.getUser = getUser;
+        this.createUser = createUser;
+        this.deleteUser = deleteUser;
+        this.updateUser = updateUser;
     }
 
     //create
+    @PostMapping("/")
+    ResponseEntity<User> newUser(@RequestBody User newUser){
+        return new ResponseEntity<>(createUser.save(newUser), HttpStatus.CREATED);
+    }
+
 
     //get
     @GetMapping("/")
@@ -26,7 +41,15 @@ public class UserRestController {
     }
 
     //delete
+    @DeleteMapping("/{id}")
+    ResponseEntity deleteUser(@PathVariable Long id){
+        deleteUser.remove(id);
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
 
     //update
-
+    @PutMapping("/{id}")
+    ResponseEntity<User> replaceUser(@RequestBody User newUser, @PathVariable Long id){
+        return new ResponseEntity<>(updateUser.update(newUser, id),HttpStatus.OK);
+    }
 }
