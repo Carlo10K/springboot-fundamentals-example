e4beb2c1103b3b2350da48f74c2e539933282e6e
Carlo10K <carlosortegadiaz50@gmail.com>
2023-01-03 18:01:05 -0600

    Paginacion

--- src/main/java/com/fundamentals/springboot/fundamentals/controller/UserRestController.java
+++ src/main/java/com/fundamentals/springboot/fundamentals/controller/UserRestController.java
@@ -5,6 +5,8 @@ import com.fundamentals.springboot.fundamentals.caseuse.DeleteUser;
 import com.fundamentals.springboot.fundamentals.caseuse.GetUser;
 import com.fundamentals.springboot.fundamentals.caseuse.UpdateUser;
 import com.fundamentals.springboot.fundamentals.entity.User;
+import com.fundamentals.springboot.fundamentals.repository.UserRepository;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,12 +21,14 @@ public class UserRestController {
 
     private final DeleteUser deleteUser;
     private final UpdateUser updateUser;
+    private final UserRepository userRepository;
 
-    public UserRestController(GetUser getUser, CreateUser createUser, DeleteUser deleteUser, UpdateUser updateUser) {
+    public UserRestController(GetUser getUser, CreateUser createUser, DeleteUser deleteUser, UpdateUser updateUser, UserRepository userRepository) {
         this.getUser = getUser;
         this.createUser = createUser;
         this.deleteUser = deleteUser;
         this.updateUser = updateUser;
+        this.userRepository = userRepository;
     }
 
     //create
@@ -51,5 +55,10 @@ public class UserRestController {
     @PutMapping("/{id}")
     ResponseEntity<User> replaceUser(@RequestBody User newUser, @PathVariable Long id){
         return new ResponseEntity<>(updateUser.update(newUser, id),HttpStatus.OK);
+    }
+
+    @GetMapping("/pageable")
+    List<User> getUserPageable(@RequestParam int page, @RequestParam int size){
+        return userRepository.findAll(PageRequest.of(page, size)).getContent();
     }
 }
